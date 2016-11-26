fe6c68fb65a8cf686667465355fd7bdcae05dd48
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2016-11-25 22:51:10 -0800

    server: create TableManager for route servers
    Preparation for single master table model.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

--- server/server.go
+++ server/server.go
@@ -93,17 +93,18 @@ type BgpServer struct {
 	fsmStateCh    chan *FsmMsg
 	acceptCh      chan *net.TCPConn
 
-	mgmtCh      chan func()
-	policy      *table.RoutingPolicy
-	listeners   []*TCPListener
-	neighborMap map[string]*Peer
-	globalRib   *table.TableManager
-	roaManager  *roaManager
-	shutdown    bool
-	watcherMap  map[WatchEventType][]*Watcher
-	zclient     *zebraClient
-	bmpManager  *bmpClientManager
-	mrtManager  *mrtManager
+	mgmtCh         chan func()
+	policy         *table.RoutingPolicy
+	listeners      []*TCPListener
+	neighborMap    map[string]*Peer
+	globalRib      *table.TableManager
+	routeServerRib *table.TableManager
+	roaManager     *roaManager
+	shutdown       bool
+	watcherMap     map[WatchEventType][]*Watcher
+	zclient        *zebraClient
+	bmpManager     *bmpClientManager
+	mrtManager     *mrtManager
 }
 
 func NewBgpServer() *BgpServer {
@@ -424,9 +425,10 @@ func (server *BgpServer) notifyBestWatcher(best map[string][]*table.Path, multip
 func (server *BgpServer) dropPeerAllRoutes(peer *Peer, families []bgp.RouteFamily) {
 
 	families = peer.toGlobalFamilies(families)
-
+	rib := server.globalRib
 	ids := make([]string, 0, len(server.neighborMap))
 	if peer.isRouteServerClient() {
+		rib = server.routeServerRib
 		for _, targetPeer := range server.neighborMap {
 			if !targetPeer.isRouteServerClient() || targetPeer == peer || targetPeer.fsm.state != bgp.BGP_FSM_ESTABLISHED {
 				continue
@@ -437,7 +439,7 @@ func (server *BgpServer) dropPeerAllRoutes(peer *Peer, families []bgp.RouteFamil
 		ids = append(ids, table.GLOBAL_RIB_NAME)
 	}
 	for _, rf := range families {
-		best, _, multipath := server.globalRib.DeletePathsByPeer(ids, peer.fsm.peerInfo, rf)
+		best, _, multipath := rib.DeletePathsByPeer(ids, peer.fsm.peerInfo, rf)
 		if !peer.isRouteServerClient() {
 			server.notifyBestWatcher(best, multipath)
 		}
@@ -518,6 +520,7 @@ func (server *BgpServer) propagateUpdate(peer *Peer, pathList []*table.Path) []*
 	}
 
 	if peer != nil && peer.isRouteServerClient() {
+		rib = server.routeServerRib
 		for _, path := range pathList {
 			path.Filter(peer.ID(), table.POLICY_DIRECTION_IMPORT)
 			path.Filter(table.GLOBAL_RIB_NAME, table.POLICY_DIRECTION_IMPORT)
@@ -1229,6 +1232,7 @@ func (s *BgpServer) Start(c *config.Global) (err error) {
 
 		rfs, _ := config.AfiSafis(c.AfiSafis).ToRfList()
 		s.globalRib = table.NewTableManager(rfs)
+		s.routeServerRib = table.NewTableManager(rfs)
 
 		if err = s.policy.Reset(&config.RoutingPolicy{}, map[string]config.ApplyPolicy{}); err != nil {
 			return
@@ -1482,6 +1486,7 @@ func (s *BgpServer) GetRib(addr string, family bgp.RouteFamily, prefixes []*tabl
 				return
 			}
 			id = peer.ID()
+			m = s.routeServerRib
 		}
 		af := bgp.RouteFamily(family)
 		tbl, ok := m.Tables[af]
@@ -1573,6 +1578,7 @@ func (s *BgpServer) GetRibInfo(addr string, family bgp.RouteFamily) (info *table
 				return
 			}
 			id = peer.ID()
+			m = s.routeServerRib
 		}
 		info, err = m.TableInfo(id, family)
 	}
@@ -1681,7 +1687,11 @@ func (server *BgpServer) addNeighbor(c *config.Neighbor) error {
 		"Topic": "Peer",
 	}).Infof("Add a peer configuration for:%s", addr)
 
-	peer := NewPeer(&server.bgpConfig.Global, c, server.globalRib, server.policy)
+	rib := server.globalRib
+	if c.RouteServer.Config.RouteServerClient {
+		rib = server.routeServerRib
+	}
+	peer := NewPeer(&server.bgpConfig.Global, c, rib, server.policy)
 	server.policy.Reset(nil, map[string]config.ApplyPolicy{peer.ID(): c.ApplyPolicy})
 	if peer.isRouteServerClient() {
 		pathList := make([]*table.Path, 0)
@@ -1694,7 +1704,7 @@ func (server *BgpServer) addNeighbor(c *config.Neighbor) error {
 		}
 		moded := server.RSimportPaths(peer, pathList)
 		if len(moded) > 0 {
-			server.globalRib.ProcessPaths(nil, moded)
+			server.routeServerRib.ProcessPaths(nil, moded)
 		}
 	}
 	server.neighborMap[addr] = peer
@@ -2485,6 +2495,7 @@ func (w *Watcher) Generate(t WatchEventType) (err error) {
 			}
 			w.notify(&WatchEventAdjIn{PathList: clonePathList(pathList)})
 		case WATCH_EVENT_TYPE_TABLE:
+			rib := w.s.globalRib
 			id := table.GLOBAL_RIB_NAME
 			if len(w.opts.tableName) > 0 {
 				peer, ok := w.s.neighborMap[w.opts.tableName]
@@ -2497,11 +2508,12 @@ func (w *Watcher) Generate(t WatchEventType) (err error) {
 					return
 				}
 				id = peer.ID()
+				rib = w.s.routeServerRib
 			}
 
 			pathList := func() map[string][]*table.Path {
 				pathList := make(map[string][]*table.Path)
-				for _, t := range w.s.globalRib.Tables {
+				for _, t := range rib.Tables {
 					for _, dst := range t.GetSortedDestinations() {
 						if paths := dst.GetKnownPathList(id); len(paths) > 0 {
 							pathList[dst.GetNlri().String()] = clonePathList(paths)
