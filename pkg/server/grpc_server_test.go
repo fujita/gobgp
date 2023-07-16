7e4d9a0e89b1fc5e4fc9865b7b6431a00dcb60e2
FUJITA Tomonori <fujita.tomonori@gmail.com>
2023-07-16 17:57:49 +0900

    add api2Path() test with binary nlri
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@gmail.com>

--- pkg/server/grpc_server_test.go
+++ pkg/server/grpc_server_test.go
@@ -1,8 +1,11 @@
 package server
 
 import (
+	"fmt"
 	"testing"
 
+	api "github.com/osrg/gobgp/v3/api"
+	"github.com/osrg/gobgp/v3/pkg/packet/bgp"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -40,4 +43,25 @@ func TestParseHost(t *testing.T) {
 			assert.Equal(t, tst.expectAddr, gotAddr)
 		})
 	}
+}
+
+func TestApi2PathWithBinaryNlri(t *testing.T) {
+	var path = &api.Path{}
+
+	path.Family = &api.Family{
+		Afi:  api.Family_AFI_IP6,
+		Safi: api.Family_SAFI_UNICAST,
+	}
+	nlri_string := "2a01:4b00:88b8:3a00:20c:29ff:fec8:20c5"
+	nexthop_string := "2a01:4b00:88b8:3a00:20c:29ff:fec8:1"
+	pr := bgp.NewIPv6AddrPrefix(128, nlri_string)
+	bp, _ := pr.Serialize()
+	path.NlriBinary = bp
+	a := bgp.NewPathAttributeMpReachNLRI(nexthop_string, []bgp.AddrPrefixInterface{pr})
+	b, _ := a.Serialize()
+	path.PattrsBinary = append(path.PattrsBinary, b)
+	p, err := api2Path(api.TableType_GLOBAL, path, false)
+	assert.NoError(t, err)
+	fmt.Println(p.GetNlri())
+	assert.Equal(t, p.GetNlri(), pr)
 }
